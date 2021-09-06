2cb7e1f82724789448e70abf82815ddfe351c35d
souravhasan992 <souravhasan992@gmail.com>
2021-09-06 14:31:49 +0600

    api loding problem solved and city name and temperature dynamically added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
-const API_KEY = `692b5e0d34357613d3a01eeebfed7208`;
+const API_KEY = `7eff4cfbdf0b5f10de1209637bf3bd12`;
 const searchTemp = () => {
-    const city = document.getElementById('city-name').value;
-    const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const cityName = document.getElementById('city-name');
+    const city = cityName.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     console.log(url)
-    
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemp(data));    
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemp = temp => {
+    setInnerText('city', temp.name);
+    setInnerText('temp', temp.main.temp);
+    console.log(temp);
 }
